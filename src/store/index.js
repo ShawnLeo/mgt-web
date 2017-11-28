fdc7fb043326d07d94abc098b13be9f8e9947650
Shawn <lishuai_247@163.com>
2017-11-28 14:42:46 +0800

    css继承、vuex继承

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import app from './app';
+import myapp from './app';
+import {app} from 'adm-portal';
 
 Vue.use(Vuex);
+app.state.system = '报表平台';
 
 const store = new Vuex.Store({
   modules: {
-    app
+    app: app,
+    myapp: myapp
   }
 });
 
